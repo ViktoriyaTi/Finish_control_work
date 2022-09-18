7d179d61059f6a96b7eb0cc471bec99cf9a3597e
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-09-18 14:00:46 +0300

    add main_program

--- main_program/Program.cs
+++ main_program/Program.cs
@@ -3,3 +3,31 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
+string[] array = new string[] { "1", "234", "floor", "cat", "2", "45", "777", "hello" };
+string[] newArray = new string[array.Length];
+GetAndFillNewArray(array, newArray);
+PrintNewArray(newArray);
+
+void GetAndFillNewArray(string[] array, string[] newArray)
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+}
+
+void PrintNewArray(string[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
